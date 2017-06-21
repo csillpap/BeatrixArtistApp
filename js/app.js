5ad09e02f8e397b24e9e55693d3ebd3a1e736773
Csilla Pap <csill.pap@gmail.com>
2017-06-21 17:20:46 +0100

    Set up routing for the different pages

---
+++ js/app.js
@@ -0,0 +1,19 @@
+angular.module("BeatrixWebsite", ["ngRoute"]);
+
+angular.module("BeatrixWebsite").config(function($locationProvider, $routeProvider) {
+	$locationProvider.html5Mode(true); //enable href routing without hashes
+	
+	$routeProvider.when("/", {
+		templateUrl: "templates/home.html"
+	})
+	.when("/gallery", {
+		templateUrl: "templates/gallery.html"
+	})
+	.when("/about", {
+		templateUrl: "templates/about.html"
+	})
+	.when("/contact", {
+		templateUrl: "templates/contact.html"
+	})
+
+});
