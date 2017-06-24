d4a978094327ccdfbc0ca5fc8fa9740b98aed411
Csilla Pap <csill.pap@gmail.com>
2017-06-24 14:01:08 +0100

    Add controller for subscribing

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,6 @@
 angular.module("Controllers", [])
+
+	//Controllers for the Contact page
 	
 	// Controller for sending messages
 	
@@ -9,6 +11,19 @@ angular.module("Controllers", [])
 			}
 			else {
 				alert("Your message could not been sent!");
+			}
+		}
+	})
+	
+	// Controller for subscribing
+	
+	.controller("SubscribeController", function($scope) {
+		$scope.submitForm = function () {
+			if ($scope.subscribeForm.$valid) {
+				alert("You have successfully subscribed!");
+			}
+			else {
+				alert("There is a problem with your request!");
 			}
 		}
 	})
