2ec754f56707d6fac576680504fd64effadb6b37
Csilla Pap <csill.pap@gmail.com>
2017-06-28 18:59:00 +0100

    Add directive for other art

--- js/directive.js
+++ js/directive.js
@@ -21,3 +21,15 @@ angular.module("PaintingsDirective",[]).directive("paintings", function() {
     templateUrl: 'templates/directives/paintings.html'
   };
 });
+
+// Directive for Other Art
+
+angular.module("OtherArtDirective",[]).directive("otherArt", function() {
+  return {
+    restrict: 'EA',    // EA -> element/attribute
+	scope: {
+		other: "="
+	},
+    templateUrl: 'templates/directives/other-art.html'
+  };
+});
